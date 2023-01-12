0173d47d03c7036297144b028cb4121d4198b72e
jimmark1 <daroyjimmarkb@gmail.com>
2023-01-13 03:27:39 +0800

    First Commit

--- library_j/library_management/doctype/library_settings/library_settings.py
+++ library_j/library_management/doctype/library_settings/library_settings.py
@@ -1,7 +1,4 @@
-# Copyright (c) 2023, Frappe and contributors
-# For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
 
 class LibrarySettings(Document):
